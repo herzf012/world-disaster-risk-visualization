027d99a25fbc875d71ac7a540b6bffcc95ff17cd
Jacob McManaman(PC) <jaycobmcm@gmail.com>
2022-10-13 19:42:08 -0500

    added .map structure for plot

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,41 +1,35 @@
 console.log("Loaded plot.js");
 
+
+
 d3.json("/wri_index").then(function (data) {
 
-    console.log(data)
+    console.log(data[0]['country_name'])
 
-    // // ... and dump that JSON to the console for inspection
-    // console.log(data);
-    // country_wri = data.map(item => {
-    //     container = {};
-    
-    //     container[item.region] = item.region;
-    //     container[item.year] = item.year;
-    //     container[item.wri] = item.wri;
+    // let years = data.map(year => year.year);
 
-    //     // Create the trace
-    //     let trace = {
-    //         x: container[item.wri],
-    //         y:container[item.year],
-    //         type: "bar"
-    //     };
+    // console.log(years)
 
-    //     // Put the trace into an array (which allows us to graph
-    //     // multiple traces, if we wish)
-    //     let traceData = [trace];
+    // Create the trace
+    let trace = {
+        x: data.map(wri => wri.wri),
+        y: data.map(year => year.year),
+        type: "bar"
+    };
 
-    //     // Define a layout object
-    //     let layout = {
-    //         title: "World Risk Index by Year",
-    //         xaxis: { title: "WRI"},
-    //         yaxis: { title: "Year"}
-    //     };
+    // Put the trace into an array (which allows us to graph
+    // multiple traces, if we wish)
+    let traceData = [trace];
 
-    //     // Create the plot
-    //     Plotly.newPlot("plot1", traceData, layout); 
+    // Define a layout object
+    let layout = {
+        title: "World Risk Index by Year",
+        xaxis: { title: "Year"},
+        yaxis: { title: "WRI"}
+    };
 
-    //     return container
-    // });
+    // Create the plot
+    Plotly.newPlot("plot1", traceData, layout); 
 
 });
 
