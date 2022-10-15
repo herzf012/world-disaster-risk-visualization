7cc3fb755a85c2c0a8b3353f6157ddc53f0f29f1
Lief Herzfeld <liefherzfeld@gmail.com>
2022-10-15 13:41:23 -0500

    Created functional ApexCharts donut chart in plot.js.

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,10 +1,12 @@
 console.log("Loaded plot.js");
 
+let user_year = "2021";
+
 
 
 d3.json("/wri_index").then(function (data) {
 
-    console.log(data[0]['country_name'])
+    console.log(data[0]['country_name']);
 
     // let years = data.map(year => year.year);
 
@@ -33,3 +35,71 @@ d3.json("/wri_index").then(function (data) {
 
 });
 
+// LIEF'S CODE BELOW
+function DrawPieChart(category, selected_year) {
+
+    console.log("DrawPieChart");
+
+    d3.json("/wri_index").then(data => {
+
+        let my_data = data.filter(element => element.year == selected_year);
+        let my_sample = my_data.map(element => element[category]);
+
+        let rank_count = {
+            very_low: 0,
+            low: 0,
+            medium: 0,
+            high: 0,
+            very_high: 0,
+            na: 0
+        };
+
+        console.log(my_sample);
+
+        for (let i = 0; i < my_sample.length; i++) {
+            if (my_sample[i] == "Very Low") {
+                rank_count["very_low"]++;
+            } else if (my_sample[i] == "Low") {
+                rank_count.low++;
+            } else if (my_sample[i] == "Medium") {
+                rank_count.medium++;
+            } else if (my_sample[i] == "High") {
+                rank_count.high++;
+            } else if (my_sample[i] == "Very High") {
+                rank_count.very_high++;
+            } else {
+                rank_count.na++;
+            };
+        };
+
+        console.log(rank_count);
+        console.log(Object.keys(rank_count));
+
+        var options = {
+            series: [rank_count.very_low, rank_count.low, rank_count.medium, rank_count.high, rank_count.very_high, rank_count.na],
+            labels: ["Very Low", "Low", "Medium", "High", "Very High", "NA"],
+            chart: {
+                type: 'donut'
+            },
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    chart: {
+                        width: 200
+                    },
+                    legend: {
+                        position: 'bottom'
+                    }
+                }
+            }]
+        };
+  
+        var chart = new ApexCharts(document.querySelector("#apex-chart"), options);
+        chart.render();
+
+    })
+
+}
+
+DrawPieChart("wri_category", user_year);
+// LIEF'S CODE ABOVE
